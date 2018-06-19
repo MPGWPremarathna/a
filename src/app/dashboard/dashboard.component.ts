2add6b70a85bc20439a4d625e66d318cbbb5950d
dinith72 <jdinith72@gmail.com>
2018-06-19 16:43:15 +0530

    test update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  chart = []; // declare chart as array
+  weeks = ['week1' , 'week2' , 'week3'];
+  earnings_cum = [1000, 2000 , 3000];
   constructor() { }
 
   ngOnInit() {
+    this.chart = new chart ('canvas', {
+      type: 'line',
+      data: {
+        labels: this.weeks,
+        datasets: [
+          {
+            data: this.earnings_cum,
+            bordercolor: '#3cba9f',
+            fill: false
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes:[{
+            display: true
+          }]
+        }
+      }
+    });
   }
+
+
 
 }
