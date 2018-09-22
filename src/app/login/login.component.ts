9e78513bfb85c11595d8867b4cce7720b42e5eec
dinith72 <jdinith72@gmail.com>
2018-09-22 21:21:42 +0530

    integratng the login screen with the backend

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import {ActivatedRoute} from '@angular/router';
 import {ApiService} from '../api.service';
 
 
@@ -12,27 +11,31 @@ import {ApiService} from '../api.service';
 export class LoginComponent implements OnInit {
   userName: String = '';
   password: String = '';
-  res: String = 'test';
-  constructor(private router: Router, private apiservice: ApiService ) { } // in the constructor following declaratin should be made
+  // res: String = 'test';
+
+  constructor(private router: Router, private apiservice: ApiService) {
+  } // in the constructor following declaratin should be made
 
   ngOnInit() {
   }
 
   onClick() {
-    var para = {
-      "un": `${this.userName}`,
-      "pw" : `${this.password}`
-    }
-    var jsonObj = JSON.stringify(para);
-    this.apiservice.postUrl('login/validate',jsonObj)
-      .subscribe((data:  Array<object>)=>{
-        console.log(data);
-      })
-    console.log(this.userName + "  " + this.password);
-    // if (this.password.match('12') && this.userName.match('dj')) {
-    //   alert('login succesful');
-    //   this.router.navigate(['dashboard']);
-    // } else {
-    //   alert('incorrect Username or password ');
-    // }
+    const para = {
+      'un': `${this.userName}`,
+      'pw': `${this.password}`
+    };
+    // const jsonObj = JSON.stringify(para);
+    this.apiservice.postUrl('login/validate', para)
+      .subscribe((data: Array<object>) => {
+        console.log(data['result']);
+        const status = data['result'];
+        if (!status.localeCompare('valid')) {
+          alert('login succesful');
+          this.router.navigate(['dashboard']);
+        } else {
+          alert('incorrect Username or password ');
+        }
+        // console.log(JSON.parse(data));
+      });
   }
+}
