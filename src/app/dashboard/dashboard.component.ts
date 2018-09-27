3204b9493d58cd626a15273beea956e5709c6a81
dinith72 <jdinith72@gmail.com>
2018-09-27 14:42:01 +0530

    creation of the current week destribution chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -28,8 +28,9 @@ import { emptyScheduled } from 'rxjs/internal/observable/empty';
 export class DashboardComponent implements OnInit {
 
   companyId = 2;
+
   // varibles regard to the autofill form
-  searchNameForm  = new FormControl();
+  searchNameForm = new FormControl();
   options: string[] = [];
   employeeInfo: object[] = [];
   filteredOptions: Observable<string[]>;
@@ -38,11 +39,18 @@ export class DashboardComponent implements OnInit {
   // date = new FormControl(moment().format('L'));
 
   //  varibles regard to  maps
-  markers  = [];
+  markers = [];
   lati: Number = 7.093543;
   long: Number = 79.9915147;
 
-  constructor(private  apiService: ApiService) { }
+  // variables related to creating of table
+  begofWeek: String;
+  endofWeek: String;
+  payments: string[] = [];
+  days: string[] = [];
+
+  constructor(private  apiService: ApiService) {
+  }
 
   ngOnInit() {
     this.getAllEmployees();
@@ -54,12 +62,13 @@ export class DashboardComponent implements OnInit {
 
     // this.getEmployeeLoc('953280086v');
   }
+
   private filter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
 
-   }
+  }
 
 
   public mapClicked(event) {
@@ -67,25 +76,24 @@ export class DashboardComponent implements OnInit {
   }
 
 
-
   private getEmployeeLoc(nic) {
-    const momentDate = moment(this.dateSel).format('YYYY-MM-DD');
+    const momentDate = moment(this.dateSel).format('YYYY-MM-DD'); // the input date is given as this.dateSel
     console.log(momentDate);
     // var date = this.dateSel.
     this.apiService.getUrl(`loc/${nic}/${momentDate}`)
-    .subscribe((locations: Array<object>) => {
-      locations.forEach((loc) => {
-        const time = loc['timestamp'].toString().substring(11, 19);
-        console.log(time);
-        const marker = {
-          lati: loc['latitude'],
-          long: loc['logitude'],
-          time: time
-        };
-        this.markers.push(marker);
+      .subscribe((locations: Array<object>) => {
+        locations.forEach((loc) => {
+          const time = loc['timestamp'].toString().substring(11, 19);
+          console.log(time);
+          const marker = {
+            lati: loc['latitude'],
+            long: loc['logitude'],
+            time: time
+          };
+          this.markers.push(marker);
+        });
+        console.log(this.markers);
       });
-      console.log(this.markers);
-    });
   }
 
   public onFindBtnClicked() {
@@ -95,33 +103,104 @@ export class DashboardComponent implements OnInit {
     let nic: String;
     this.employeeInfo.forEach((emp) => {
       console.log(emp);
-      if (this.nameSel ==  emp['name']) {
+      if (this.nameSel === emp['name']) {
         nic = emp['nic'];
-
-
       }
     });
     this.getEmployeeLoc(nic);
-    console.log(nic);
+    this.getEmployeeChartData(nic);
+    // console.log(nic);
   }
 
   private getAllEmployees() {
     this.apiService.getUrl(`emp/${this.companyId.toString()}`)
-    .subscribe((employees: Array<object>) => {
-      employees.forEach((emp) => {
+      .subscribe((employees: Array<object>) => {
+        employees.forEach((emp) => {
 
-        console.log();
+          console.log();
 
-        this.options.push(emp['firstName'] + ' ' + emp['lastName']);
-        const empInfo = {
-          nic: emp['NIC'],
-          name: emp['firstName'] + ' ' + emp['lastName']
+          this.options.push(emp['firstName'] + ' ' + emp['lastName']);
+          const empInfo = {
+            nic: emp['NIC'],
+            name: emp['firstName'] + ' ' + emp['lastName']
 
-        };
-        this.employeeInfo.push(empInfo);
+          };
+          this.employeeInfo.push(empInfo);
+        });
+        console.log(this.employeeInfo);
       });
-      console.log(this.employeeInfo);
-    });
   }
 
+
+// chart cretion functions
+  private getEmployeeChartData(nic) {
+    this.endofWeek = moment(this.dateSel).endOf('week').format('YYYY-MM-DD');
+    this.begofWeek = moment(this.dateSel).startOf('week').format('YYYY-MM-DD');
+    let day = this.begofWeek;
+    for (let i = 0; i < 7; i++) {
+      this.apiService.getUrl(`pmt/${nic}/${day}`)
+        .subscribe((payment) => {
+          console.log(payment['pmnt']);
+
+          if (payment['pmnt']) {
+            this.payments.push(payment['pmnt'].toString());
+          } else {
+            this.payments.push('0.00');
+          }
+          if (i === 6) {
+            this.createCollectionChart();
+          }
+
+        });
+      this.days.push(day.toString());
+      day = moment(day).add(1, 'day').format('YYYY-MM-DD');
+      // console.log(day);
+    }
+
+    console.log(this.payments);
+    console.log(this.days);
+    // console.log(this.endofWeek);
+    // console.log(this.begofWeek);
+  }
+
+  private createCollectionChart() {
+    // creating the new chart
+    this.LineChart = new Chart('LineChartCollections',
+      {
+        type: 'bar',
+        data: {
+          labels: this.days,
+          datasets: [{
+            label: 'amount paid',
+            data: this.payments,
+            fill: false,
+            lineTension: 0.2,
+            borderColor: 'red',
+            borderWidth: 1,
+          }],
+        },
+        options: {
+          title: {
+            text: 'Current Week collection Destribution',
+            display: true
+          },
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+              }
+            }],
+            xAxes: [{
+              type: 'time',
+              time: {
+                unit: 'day',
+                min: this.begofWeek,
+                max: this.endofWeek,
+              }
+            }]
+          }
+        },
+
+      });
+  }
 }
